1bfb67ea7f0ced730cf4fb212701329ed0c4b4a0
MahidulParsh <mahidulparash44@gmail.com>
2021-08-31 02:28:14 +0600

    evaluation 1

--- app/src/main/java/com/example/ninjamoney/MainActivity.java
+++ app/src/main/java/com/example/ninjamoney/MainActivity.java
@@ -23,8 +23,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         setup();
 
-        //Intent intentInc = new Intent(this, Login.class);
-        //startActivity(intentInc);
+        Intent intentInc = new Intent(this, Login.class);
+        startActivity(intentInc);
     }
 
     private void setup(){
