d3a89516c6466c325b9934bc1c138efbdbfef6a6
MahidulParsh <mahidulparash44@gmail.com>
2021-10-03 16:04:14 +0600

    balance Window w/o database

--- app/src/main/java/com/example/ninjamoney/Balance.java
+++ app/src/main/java/com/example/ninjamoney/Balance.java
@@ -9,11 +9,19 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.example.ninjamoney.LoginSignUp.Login;
 import com.example.ninjamoney.LoginSignUp.Profile;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class Balance extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -21,15 +29,83 @@ public class Balance extends AppCompatActivity implements NavigationView.OnNavig
     NavigationView navigationView;
     Toolbar toolbar;
 
+    TextView balance_tv;
+    TextView cash_balance_tv;
+    TextView bank_balance_tv;
+    TextView mobile_balance_tv;
+    TextView username_tv;
+    TextView email_tv;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase database;
+    private DatabaseReference dRef;
+    private FirebaseUser firebaseUser;
+    String uid;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_balance);
         setup();
         drawer();
+        firebase();
+        update();
+    }
+
+    private void firebase() {
+        firebaseAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        uid = firebaseUser.getUid();
+        dRef = database.getReference().child("Users").child(uid);
+
+        Query query = dRef.orderByChild("email");
+
+        query.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot snapshot, String previousChildName) {
+                if (snapshot.exists()) {
+                    String name = snapshot.child("username").getValue().toString();
+                    String email = snapshot.child("email").getValue().toString();
+                    username_tv.setText(name);
+                    email_tv.setText(email);
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot snapshot, String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot snapshot, String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void update() {
+
     }
 
     private void setup() {
+        balance_tv = findViewById(R.id.balance_tv);
+        cash_balance_tv = findViewById(R.id.cash_balance_tv);
+        bank_balance_tv = findViewById(R.id.bank_balance_tv);
+        mobile_balance_tv = findViewById(R.id.mobile_balance_tv);
+        username_tv = findViewById(R.id.username_tv);
+        email_tv = findViewById(R.id.email_tv);
+
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
         toolbar = findViewById(R.id.toolbar);
